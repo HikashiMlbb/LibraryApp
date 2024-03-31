4e19a0aa2ae9858ec524162f7d0950e135080794
Roman <hikashimlbb@gmail.com>
2024-03-31 21:23:32 +0700

    Added Validation Behavior

--- src/LibraryApp.Application/DependencyInjection.cs
+++ src/LibraryApp.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using LibraryApp.Application.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LibraryApp.Application;
@@ -6,7 +7,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
         return services;
     }
 }
